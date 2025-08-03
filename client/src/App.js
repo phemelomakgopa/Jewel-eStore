650a3f4e35677773720117fd8bb6496fbc073894
QueenMohlala1 <147653174+QueenMohlala1@users.noreply.github.com>
2025-08-03 13:46:15 +0200

    React and TailwindCSS setup and configuration for webpage design

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      {/* Next: Hero section */}
+    </div>
+  );
+}
+
+export default App;
